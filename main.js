bf97f44020840e66d8c52dee4a215beff5e639c8
E-Viskhan <viskhan11@gmail.com>
2021-11-26 01:31:56 +0300

    Исправил свое несоответствие заданию

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-let title = "Какой-то title";
-let screens = ['1920', '1280'];
-let screenPrice = 500;
-let rollback = 1;
-let fullPrice = 1;
-let adaptive = 1;
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
 
 console.log("Сообщение для консоли");
 alert("Привет, это модальное окно");
