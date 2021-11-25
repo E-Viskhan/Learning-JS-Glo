0bebf1b570325a523bfe009df0695cf4272273b2
E-Viskhan <viskhan11@gmail.com>
2021-11-26 01:28:02 +0300

    Первое ДЗ на курсе

---
+++ main.js
@@ -0,0 +1,9 @@
+let title = "Какой-то title";
+let screens = ['1920', '1280'];
+let screenPrice = 500;
+let rollback = 1;
+let fullPrice = 1;
+let adaptive = 1;
+
+console.log("Сообщение для консоли");
+alert("Привет, это модальное окно");
