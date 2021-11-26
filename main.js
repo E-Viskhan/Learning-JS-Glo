cf9e5594a0a406821a8cb762f3b1174531d8fc0b
E-Viskhan <viskhan11@gmail.com>
2021-11-26 18:59:43 +0300

    Выполнена вторая домашняя работа!

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Расчет стоимости работы";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 5000;
+let rollback = 15;
+let fullPrice = 100000;
+let adaptive = true;
 
-console.log("Сообщение для консоли");
-alert("Привет, это модальное окно");
+// Вывод в консоль типов данных переменных
+console.log("Тип данных для переменной title:", typeof title);
+console.log("Тип данных для переменной fullPrice:", typeof fullPrice);
+console.log("Тип данных для переменной adaptive:", typeof adaptive);
+
+// Вывод в консоль длину строки screens
+console.log("Длина строки переменной screens:", screens.length);
+
+// Вывод в консоль стоимости верстки экранов и стоимость верстки сайта
+console.log(`Стоимость верстки экранов ${screenPrice} рублей и стоимость разработки сайта ${fullPrice}`);
+
+// Приведение строки screens к нижнему регистру, разбиение строки на массив, выведение массива в консоль
+screens = screens.toLowerCase(); // приведение в нижний регистр
+screens = screens.split(', '); // разбиение строки по запятым с пробелом
+console.log(screens);
+
+// Вывод в консоль процента отката посреднику за работу (fullPrice * (rollback/100))
+console.log("Процент отката:", fullPrice * (rollback / 100));
