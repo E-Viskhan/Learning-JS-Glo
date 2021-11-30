b381357f6e6c492e1f86c6a3554c2ce230986a98
E-Viskhan <viskhan11@gmail.com>
2021-11-30 18:05:06 +0300

    fix: Улучшен code-style

--- main.js
+++ main.js
@@ -11,27 +11,6 @@ let fullPrice = screenPrice + firstServiePrice + secondServiePrice;
 let servicePercentPrice = Math.ceil((100 - rollback) / 100 * fullPrice);
 let discountMessage;
 
-console.log('Итоговая стоимость, за вычетом отката посреднику:', servicePercentPrice);
-
-// Вывод в консоль типов данных переменных
-console.log("Тип данных для переменной title:", typeof title);
-console.log("Тип данных для переменной fullPrice:", typeof fullPrice);
-console.log("Тип данных для переменной adaptive:", typeof adaptive);
-
-// Вывод в консоль длину строки screens
-console.log("Длина строки переменной screens:", screens.length);
-
-// Вывод в консоль стоимости верстки экранов и стоимость верстки сайта
-console.log(`Стоимость верстки экранов ${screenPrice} рублей и стоимость разработки сайта ${fullPrice}`);
-
-// Приведение строки screens к нижнему регистру, разбиение строки на массив, выведение массива в консоль
-screens = screens.toLowerCase(); // приведение в нижний регистр
-screens = screens.split(', '); // разбиение строки по запятым с пробелом
-console.log(screens);
-
-// Вывод в консоль сумма отката посреднику за работу (fullPrice * (rollback/100))
-console.log("Сумма отката:", fullPrice * (rollback / 100));
-
 if (fullPrice <= 0) {
   discountMessage = 'Что то пошло не так';
 } else if (fullPrice > 0 && fullPrice < 15000) {
@@ -42,4 +21,17 @@ if (fullPrice <= 0) {
   discountMessage = 'Даем скидку в 10%';
 }
 
+console.log('Итоговая стоимость, за вычетом отката посреднику:', servicePercentPrice);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей и стоимость разработки сайта ${fullPrice}`);
+console.log("Сумма отката:", fullPrice * (rollback / 100));
 console.log(discountMessage);
+
+// Вывод в консоль типов данных переменных
+console.log("Тип данных для переменной title:", typeof title);
+console.log("Тип данных для переменной fullPrice:", typeof fullPrice);
+console.log("Тип данных для переменной adaptive:", typeof adaptive);
+
+console.log("Длина строки переменной screens:", screens.length);
+screens = screens.toLowerCase(); // приведение в нижний регистр
+screens = screens.split(', '); // разбиение строки по запятым с пробелом
+console.log(screens);
